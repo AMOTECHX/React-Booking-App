a8654d89ca8fbcf33b9941f4c5368d70675c63b9
AMOTECHX <94161414+AMOTECHX@users.noreply.github.com>
2022-05-03 10:15:31 +0300

    upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          
+        </ul>
+        <ul className="fList">
+         
+        </ul>
+        <ul className="fList">
+          
+        </ul>
+        <ul className="fList">
+         
+        </ul>
+        <ul className="fList">
+          
+        </ul>
+      </div>
+      <div className="fText">Copyright © 2022 Amotechx.</div>
+    </div>
+  );
+};
+
+export default Footer;
